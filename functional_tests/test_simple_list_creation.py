8b2994cc418814358ff1efbbbde058a10fd8d9ed
adn <adndgk@gmail.com>
2021-02-23 20:45:34 -0300

    Refactor list view to handle new item POSTs

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -2,6 +2,7 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from unittest import skip
 
 class NewVisitorTest(FunctionalTest):
 
@@ -33,7 +34,7 @@ class NewVisitorTest(FunctionalTest):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
-        #  methodical)
+        # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
