00f8dd428d51d8b7d651c093405c48a91d64c1fa
adn <adndgk@gmail.com>
2021-02-28 18:53:25 -0300

    Remove fail in test_login

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -53,7 +53,6 @@ class LoginTest(FunctionalTest):
 
         # She is logged out
         self.wait_to_be_logged_out(email=test_email)
-        self.fail()
     
     def wait_for_email(self, test_email, subject):
         if not self.staging_server:
